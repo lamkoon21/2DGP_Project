dd4e1b5fc945fb67fb6f737f9a46589e160bf737
lamkoon21 <mezziroo@gmail.com>
2022-12-04 08:20:41 +0900

    스테이지, 세이브 포인트 저장값 추가

--- server.py
+++ server.py
@@ -3,5 +3,13 @@ spike = None
 soul = None
 background = None
 bgm = None
+visit = [0,0,0,0,0,0,0,0]
+
+key_in_stage = [0, 0, 0]
+
+pre_stage = 4
+current_stage = 5
+
+save_point = 1
 
 collide_box = True
