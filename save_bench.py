70e97ac4b62c84b8095cc457ae9d71f89eabbe7c
lamkoon21 <mezziroo@gmail.com>
2022-12-04 09:53:33 +0900

    세이브 벤치에 세이브 포인트 표시 추가

--- save_bench.py
+++ save_bench.py
@@ -1,4 +1,4 @@
-from pico2d import draw_rectangle
+from pico2d import draw_rectangle, load_font
 import server
 
 class Savebench:
@@ -6,14 +6,24 @@ class Savebench:
         self.left, self.bottom, self.right, self.top = 0, 0, 0, 0
         self.x1, self.y1, self.x2, self.y2 = x1, y1, x2, y2
         
+        self.font = load_font('font.ttf', 32)
+        self.write = False
+        
         
     def update(self):
         self.left, self.top = self.x1 - server.background.window_left, self.y1 - server.background.window_bottom
-        self.right, self.bottom = self.x2 - server.background.window_left, self.y2 - server.background.window_bottom 
+        self.right, self.bottom = self.x2 - server.background.window_left, self.y2 - server.background.window_bottom
+        
+        if server.knight.x < self.x2 and server.knight.x > self.x1:
+            self.write = True
+        else: self.write = False
 
     def draw(self):
         if server.collide_box:
-                draw_rectangle(*self.get_bb())
+            draw_rectangle(*self.get_bb())
+        
+        if self.write:
+            self.font.draw((self.left+ self.right) / 2 - 80, self.top + 100, f'SAVE POINT', (255,255,255))
         
     def get_bb(self):
         return self.left, self.bottom, self.right, self.top
